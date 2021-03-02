2c11406bc1e6a38f87a2356fdabf96c0e6e975b8
Jan Lana <lana.jan@gmail.com>
2021-03-02 16:28:19 +0100

    add seckey generation

--- app/src/main/java/name/lana/dodo/MainActivity.java
+++ app/src/main/java/name/lana/dodo/MainActivity.java
@@ -1,36 +1,86 @@
 package name.lana.dodo;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog.Builder;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static PrivateKey privateKey;
+    private static PublicKey publicKey;
+
+    private void initKey() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, UnrecoverableEntryException, NoSuchProviderException, InvalidAlgorithmParameterException {
+        Log.v(TAG, "initKeystore");
+
+        String keyAlias = getString(R.string.message_key_alias);
+
+        KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
+        ks.load(null);
+
+        KeyStore.Entry entry = ks.getEntry(keyAlias, null);
+        if (entry == null) {
+            Log.v(TAG, "initKeystore: generate new key");
+
+            /* generate new key */
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_EC, "AndroidKeyStore");
+            kpg.initialize(new KeyGenParameterSpec.Builder(
+                    keyAlias,
+                    KeyProperties.PURPOSE_SIGN | KeyProperties.PURPOSE_VERIFY)
+                    .setDigests(KeyProperties.DIGEST_SHA256,
+                            KeyProperties.DIGEST_SHA512)
+                    .build());
+
+            KeyPair kp = kpg.generateKeyPair();
+            entry = ks.getEntry(keyAlias, null);
+        }
+
+        privateKey = ((KeyStore.PrivateKeyEntry) entry).getPrivateKey();
+        publicKey = ks.getCertificate(keyAlias).getPublicKey();
+
+        Log.v(TAG, String.valueOf(publicKey));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.v(TAG, "onCreate");
+
+        try {
+            initKey();
+        } catch (Exception e) {
+            Log.e(TAG, "key initialization failed", e);
+        }
 
         SharedPreferences prefs =
                 PreferenceManager.getDefaultSharedPreferences(this);
@@ -38,8 +88,6 @@ public class MainActivity extends AppCompatActivity {
         prefs.registerOnSharedPreferenceChangeListener(
                 new SharedPreferences.OnSharedPreferenceChangeListener() {
                     public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
-                        Log.v(TAG, "change " + key);
-
                         if (key.equals("mode"))
                             updateMode(prefs);
                     }
@@ -58,11 +106,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateMode(SharedPreferences prefs) {
         boolean isMaster = prefs.getBoolean("mode", false);
+
+        Log.v(TAG, "mode update to: " + (isMaster?"MASTER":"SLAVE"));
+
         if (isMaster) {
             setContentView(R.layout.activity_main_master);
         } else {
             setContentView(R.layout.activity_main_slave);
-
 
             FirebaseMessaging.getInstance().getToken()
                     .addOnCompleteListener(new OnCompleteListener<String>() {
@@ -75,16 +125,11 @@ public class MainActivity extends AppCompatActivity {
 
                             // Get new FCM registration token
                             String token = task.getResult();
-
-                            // Log and toast
-                            // String msg = getString(R.string.msg_token_fmt, token);
                             Log.d(TAG, token);
                             Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                         }
                     });
-
         }
-
     }
 
     @Override
@@ -95,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.action_settings:
                 startActivity(new Intent(this, MainSettingsActivity.class));
                 return true;
@@ -104,7 +149,4 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
-
-
 }
